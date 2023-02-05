b7d67a58bad570861ee26f400e1fc68e380ae928
jack <jack.chenyuana@gmail.com>
2023-02-05 21:24:48 +0800

    add svg-loader

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,35 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/')
+    },
+    configure: webpackConfig => {
+      const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf)
+      if (oneOfRule) {
+        oneOfRule.oneOf.splice(0, 0, {
+          test: /\.svg$/,
+          use: [{
+            loader: 'svg-sprite-loader',
+            options: {
+              extract: false,
+            }
+          },
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                {
+                  name: 'removeAttrs',
+                  params: {
+                    attrs: 'fill'
+                  }
+                }
+              ]
+            }
+          }
+          ]
+        })
+      }
+      return webpackConfig
     }
   },
   plugins: []
